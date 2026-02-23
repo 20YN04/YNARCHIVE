d2ea9db90ce21d4d8ab6679c19c95addde49b3fe
20YN04 <Yentl.nerinckx@icloud.com>
2026-02-23 21:51:02 +0100

    Implement layout component and update app structure

--- PORTFOLIO/src/app/app.ts
+++ PORTFOLIO/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LayoutComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
